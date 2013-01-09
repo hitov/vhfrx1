2726831847ffbb2458daef6ac58fec58e001f5ea
Angel Hitov <angelhitov@gmail.com>
2013-01-09 23:11:10 +0200

    Make Leds to blink
    Signed-off-by: Angel Hitov <angelhitov@gmail.com>

--- src/vhf-radio.c
+++ src/vhf-radio.c
@@ -5,9 +5,13 @@
 int main(void)
 {
     DDRC = 0x06;
-    PORTC = 0;
+    PORTC = 1<<1;
     for(;;)
     {
+        PORTC = 1<<1;
+        _delay_ms(100);
+        PORTC = 2<<1;
+        _delay_ms(100);
     }
     return 0;
 }
